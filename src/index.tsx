b258d833fe37cd3461d87584d3fa7f594645495d
Egor <mikheevev@lad24.ru>
2019-09-16 09:16:32 +0300

    imporove performance 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,30 @@
-import React, { lazy, Suspense } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom';
-// import { RouterContainer } from 'containers/Router';
+import { RouterContainer } from 'containers/Router';
 
 
-const RouterContainer = lazy(() =>
-    import('./containers/Router')
-        .then(({ RouterContainer }) => ({ default: RouterContainer })),
-);
+// const RouterContainer = lazy(() =>
+//     import('./containers/Router')
+//         .then(({ RouterContainer }) => ({ default: RouterContainer })),
+// );
 
-const preloader = () => {
-    return (<div className="banter-loader">
-        <div className="banter-loader__box" />
-        <div className="banter-loader__box" />
-        <div className="banter-loader__box" />
-        <div className="banter-loader__box" />
-        <div className="banter-loader__box" />
-        <div className="banter-loader__box" />
-        <div className="banter-loader__box" />
-        <div className="banter-loader__box" />
-        <div className="banter-loader__box" />
-    </div>)
-}
+// const preloader = () => {
+//     return (<div className="banter-loader">
+//         <div className="banter-loader__box" />
+//         <div className="banter-loader__box" />
+//         <div className="banter-loader__box" />
+//         <div className="banter-loader__box" />
+//         <div className="banter-loader__box" />
+//         <div className="banter-loader__box" />
+//         <div className="banter-loader__box" />
+//         <div className="banter-loader__box" />
+//         <div className="banter-loader__box" />
+//     </div>)
+// }
 
 const container = document.getElementById("root")
-ReactDOM.render(<Suspense fallback={ preloader } ><RouterContainer /></Suspense>, container)
+// ReactDOM.render(<Suspense fallback={ preloader } ><RouterContainer /></Suspense>, container)
+ReactDOM.render(<RouterContainer />, container)
 
 // позволяет выполнить функцию жизненного цикла componentWillUnmount при закрытии приложения.
 // в нашем случае она скидывает дамп редак стора в локальное хранилище
